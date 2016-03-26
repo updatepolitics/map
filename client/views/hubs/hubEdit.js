accb99833b57bebcdf73103a48b305f215f0e25c
Vitor George <vitor.george@gmail.com>
2016-03-26 10:58:29 -0300

    incidence reach field

--- client/views/hubs/hubEdit.js
+++ client/views/hubs/hubEdit.js
@@ -4,10 +4,14 @@ Template.hubEdit.onCreated(function (){
 
 Template.hubEdit.helpers({
   countryOptions: function(){
-
     return Countries.find({}, {sort: {en: 1}}).map(function (c) {
       return {label: c.en, value: c._id};
     });
-
+  },
+  incidencyReachOptions: function(){
+    return _.map(['Local', 'National', 'Regional', 'International'], function (i) {
+      return {label: i, value: i};
+    });
+    return ;
   }
 });
