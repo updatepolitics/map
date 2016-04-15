dda8248ad26a47c90e98873c59654c499b7ce076
Vitor George <vitor.george@gmail.com>
2016-04-15 12:32:27 -0300

    better login flow

--- router/config.js
+++ router/config.js
@@ -1,3 +1,18 @@
 Router.configure({
   layoutTemplate: 'appLayout'
 });
+
+AccountsTemplates.configure({
+  defaultLayout: 'authLayout',
+  onLogoutHook: function() {
+    Router.go('/login');
+  }
+});
+
+AccountsTemplates.configureRoute('signIn', {
+    name: 'signin',
+    path: '/login',
+    template: 'login',
+    layoutTemplate: 'authLayout',
+    redirect: '/admin',
+});
