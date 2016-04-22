9b7bc7b03c7ecfd7a41439cfee35dbda61951367
Vitor George <vitor.george@gmail.com>
2016-04-22 17:55:28 -0300

    configure i18n for menu items

--- client/views/shared/appMenu.js
+++ client/views/shared/appMenu.js
@@ -5,42 +5,81 @@ Template.appMenu.events({
   "mouseleave #menu": function(event, template){
     $('#menu').animate({left: (-1 * $('#menu').width() )}, 350, 'swing');
   },
-
+  "click .bt": function(event, template){
+    Router.go(this.route);
+  },
+  "click .sub_bt": function(event, template){
+    Router.go(this.route, {}, {hash: this.anchor});
+  },
 });
 
 Template.appMenu.helpers({
+  menuItemLabel: function(route) {
+    return 'menu.items.' + this.route;
+  },
+  subMenuItemLabel: function(route) {
+    return 'menu.sub_items.' + this.route + '.' + this.anchor;
+  },
+  isSelected: function() {
+    if (Router.current().route.getName(this) == self.route) {
+      return 'selected'
+    }
+  },
   menuItens: function(){
-    return [
-      { "label": "HOME", "route": "home" },
-      { "label": "UPDATE", "route": "about", "subItems" : [
-        {"label": "About Update", "anchor": "a_intro" },
-        {"label": "Context", "anchor": "a_context" },
-        {"label": "Team", "anchor": "a_who" },
-        {"label": "Sponsors", "anchor": "a_financer" },
-        {"label": "Transparency", "anchor": "a_transparency" },
-        {"label": "Partners", "anchor": "a_partners" }
-      ]},
-      { "label": "MAPPING", "_en": "MAPPING", "route": "mapping", "subItems" : [
-         {"label": "About", "anchor": "a_about" },
-         {"label": "Methodology", "anchor": "a_methodology" },
-         {"label": "Scope", "anchor": "a_coverage" },
-         {"label": "Mapping team", "anchor": "a_staff" },
-         {"label": "Software", "anchor": "a_developers" },
-         {"label": "Partners", "anchor": "a_partners" }
-      ]},
-      { "label": "HUBS", "route": "hubs", "subItems" : [
-         {"label": "Introduction", "anchor": "intro" },
-         {"label": "Nature", "anchor": "kind" },
-         {"label": "Sponsor", "anchor": "financer" }
-      ]},
-      { "label": "SIGNALS", "route": "signals", "subItems" : [
-         {"label": "Introduction", "anchor": "intro" },
-         {"label": "Tecnology", "anchor": "tech" },
-         {"label": "Themes", "anchor": "theme" },
-         {"label": "Purpose", "anchor": "purpose" },
-         {"label": "Methods", "anchor": "mechanism" }
-      ]},
-      { "label": "EXPLORE", "route": "EXPLORE"}
-    ];
+    return [{
+      route: 'home'
+    }, {
+      route: 'about',
+      anchors: [
+        'a_intro',
+        'a_context',
+        'a_who',
+        'a_financer',
+        'a_transparency',
+        'a_partners'
+      ]
+    }, {
+      route: 'mapping',
+      anchors: [
+        'about',
+        'methodology',
+        'coverage',
+        'staff',
+        'developers',
+        'partners'
+      ]
+    }, {
+      route: 'hubs',
+      anchors: [
+          'a_intro',
+          'a_kind',
+          'a_sponsor'
+      ]
+    }, {
+      route: 'signals',
+      anchors: [
+        "intro",
+        "tech",
+        "theme",
+        "purpose",
+        "mechanism"
+      ]
+    }, {
+      route: 'download'
+    }, {
+      route: 'explore'
+    }];
+  },
+  anchors: function() {
+    // Will pass route parameter along with anchor
+    var self = this;
+    if (Router.current().route.getName(this) == self.route) {
+      return _.map(this.anchors, function(anchor){
+        return {
+          route: self.route,
+          anchor: anchor
+        };
+      });
+    }
   }
 });
