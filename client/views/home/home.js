f38bb1f25f67743e21e212a24a6a4ced572618c2
Vitor George <vitor.george@gmail.com>
2016-05-05 22:26:22 -0300

    presentation in portuguese

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,3 +1,41 @@
+var duration = 350;
+
+var modalPages = [
+  {
+    title: "A Democracia na América Latina do séc XXI",
+    text: "A democracia ainda está em processo de consolidação e amadurecimento na América Latina. A fragilidade das instituições democráticas, ameaças a direitos fundamentais e a interferência de diferentes tipos de elites nas decisões políticas influenciam diretamente a percepção e a confiança dos cidadãos no processo político, principalmente numa sociedade cada vez mais conectada e participativa."
+  },
+  {
+    title: "Uma sociedade conectada e participativa",
+    text: "As dinâmicas da sociedade estão mudando mais intensamente no século XXI. As formas tradicionalmente conhecidas se somam às novas formas de produzir, consumir, comunicar e se relacionar. O acesso e distribuição do poder também são impactados por essas mudanças. O contexto de uma democracia em construção e as novas dinâmicas sociais possibilitam a emergência de novas práticas políticas."
+  },
+  {
+    title: "A emergência de práticas políticas",
+    text: "Essas práticas atuam na fronteira dos processos democráticos, expandindo os limites de interação entre a sociedade civil e o poder público. Em todo o mundo, os atores do campo de inovação política estão desenvolvendo experimentos de diferentes formas e em diferentes contextos. Na América Latina, observamos que esses experimentos apontam possibilidades de caminhos para atualizar a política do século XXI."
+  },
+  {
+    title: "Ecossistema de práticas políticas emergentes",
+    text: "O Update acredita que retratar e criar uma visualização para o ecossistema de práticas políticas emergentes é o primeiro passo para subsidiar a afirmação da existência desse campo e, a partir dessas evidências, potencializar uma visão de coletividade e pertencimento para os atores envolvidos nos processos políticos que contribuem para a redução da distância entre a sociedade civil e o poder público."
+  },
+  {
+    title: "A plataforma",
+    text: "Para dar luz a esse ecossistema, desenvolveu-se uma classificação própria representada nesta plataforma digital. São 700 iniciativas mapeadas, de 21 países da América Latina. Para ter visão das precisa sobre as práticas, as informações foram separada em duas formas diferentes de visualização. A primeira é por meio dos Hubs - os atores do ecossistema. A segunda é por meio dos sinais - são as práticas e projetos realizados pelos hubs. "
+  },
+  {
+    title: "Hubs e sinais, os atores e as práticas do ecossistema",
+    text: "Os hubs são organismos - institucionalizados ou não - que desenvolvem ações e funcionam como emissores dos sinais de inovação. Cada hub é capaz de realizar diversos sinais, os quais possuem métodos, abordagens e propósitos distintos. Olhar para os hubs sem especificar suas práticas e táticas de ação resultaria numa visão genérica e não ecossistêmica. Além disso, a observação a partir dos sinais permite compreender quais são as práticas mais utilizadas e de que maneira o ecossistema se desenvolve no sentido de reduzir a distância entre sociedade civil e poder público. "
+  },
+  {
+    title: "Explore e conheça o ecossistema",
+    text: "A proposta dessa plataforma é permitir que se conheça um pouco do universo das novas práticas políticas. O convite é para explorar os resultados variando as múltiplas combinações de filtros e permitindo análises próprias. Esse mapeamento é o primeiro esforço para compreender esse complexo ecossistema de natureza altamente dinâmica, onde iniciativas nascem e morrem do dia pra noite, e onde os contextos (sociais, políticos e econômicos) influenciam diretamente nas práticas locais."
+  }
+]
+
+Template.home.onCreated(function(){
+  this.showModal = new ReactiveVar(false);
+  this.modalPage = new ReactiveVar(1);
+});
+
 Template.home.onRendered(function (){
   $('#update_logo').hide()
 });
@@ -6,8 +44,54 @@ Template.home.onDestroyed(function (){
   $('#update_logo').show()
 });
 
+Template.home.helpers({
+  modalPage: function(){
+    return Template.instance().modalPage.get();
+  },
+  modalTitle: function() {
+    var page = Template.instance().modalPage.get();
+    return modalPages[page - 1].title;
+  },
+  modalText: function() {
+    var page = Template.instance().modalPage.get();
+    return modalPages[page - 1].text;
+  }
+});
+
 Template.home.events({
   "click #explore": function(event, template){
      Router.go('explore');
+  },
+  "click #about": function(event, template){
+    $(curtain).fadeIn(duration);
+    $(modal_home).fadeIn(duration);
+    $(update_logo_home).fadeOut(duration);
+    $(quotes).fadeOut(duration);
+    $(credit).fadeOut(duration);
+    $(about).fadeOut(duration);
+    $(explore).fadeOut(duration);
+  },
+  "click #modal_home_x": function(event, template) {
+    $(curtain).fadeOut(duration);
+  	$(modal_home).fadeOut(duration);
+  	$(update_logo_home).fadeIn(duration);
+  	$(quotes).fadeIn(duration);
+  	$(credit).fadeIn(duration);
+  	$(about).fadeIn(duration);
+  	$(explore).fadeIn(duration);
+  },
+  "click #modal_home_next": function(event, template) {
+    var page = template.modalPage.get();
+    if (page < 7) page += 1;
+    if (page == 7) $(modal_home_next).css({opacity: 0.2, cursor: 'default'});
+    if (page == 2) $(modal_home_prev).css({opacity: 1, cursor: 'pointer'});
+    template.modalPage.set(page);
+  },
+  "click #modal_home_prev": function(event, template) {
+    var page = template.modalPage.get();
+    if (page > 1) page -= 1;
+    if (page == 1) $(modal_home_prev).css({opacity: 0.2, cursor: 'default'});
+    if (page == 6) $(modal_home_next).css({opacity: 1, cursor: 'pointer'});
+    template.modalPage.set(page);
   }
 });
