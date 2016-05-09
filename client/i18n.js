00925889e67c4836e20b2c3ff3de8326370f1911
Vitor George <vitor.george@gmail.com>
2016-05-09 01:48:45 -0300

    check header for accepted language

--- client/i18n.js
+++ client/i18n.js
@@ -1,18 +1,17 @@
-getUserLanguage = function () {
-  // Put here the logic for determining the user language
-
-  return "en";
-};
-
 Meteor.startup(function () {
-  Session.set("showLoadingIndicator", true);
+  headers.ready(function() {
+    var acceptLanguage = headers.get('accept-language');
+    var acceptedLanguages = [];
 
-  TAPi18n.setLanguage(getUserLanguage())
-    .done(function () {
-      Session.set("showLoadingIndicator", false);
-    })
-    .fail(function (error_message) {
-      // Handle the situation
-      console.log(error_message);
+    _.each(['pt', 'es', 'en'], function(language){
+      var index = acceptLanguage.indexOf(language);
+      if (index > -1) acceptedLanguages.push({language: language, index: index});
     });
+
+    acceptedLanguages = _.sortBy(acceptedLanguages, function(l){ return l.index });
+
+    if (acceptedLanguages.length > 0) return TAPi18n.setLanguage(acceptedLanguages[0].language);
+    else return TAPi18n.setLanguage('en');
+  });
+
 });
